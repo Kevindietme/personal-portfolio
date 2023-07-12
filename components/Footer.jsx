c6ca874add36175f18b9c2c7337cc72218afbcb0
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-12 16:58:43 -0400

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+  const currentYear = new Date().getFullYear()
+  const gitHubUrl= "https://github.com/kevindietme"
+  return (
+      <footer>
+          <p className="text-slate-100">&copy;{currentYear}</p>
+          <p><a href={gitHubUrl} target="_blank" rel="noreferrer" className="text-slate-100">See Code in Github</a></p>
+      </footer>
+  )
+}
